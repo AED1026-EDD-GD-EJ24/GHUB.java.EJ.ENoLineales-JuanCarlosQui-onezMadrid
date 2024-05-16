065cca38e1104ec86e69fa09c528e8c168a71ae2
Juan Carlos Quiñonez Madrid <149096035+CarlosMadrid11@users.noreply.github.com>
2024-05-16 16:13:07 +0000

    arbolAVL

--- arbolAVL/Logical.java
+++ arbolAVL/Logical.java
@@ -1,5 +1,20 @@
 package arbolAVL;
+/*
+ * Determina si se debe cambiar la altura de un nodo
+ * Es utilizado por la calse ArbolAVL
+ */
 
-public class Logical {
-    
+ public class Logical {
+	boolean v;
+	public Logical (boolean f) {
+		v=f;
+	}
+	public void setLogical(boolean f) {
+		v=f;
+	}
+	public boolean booleanValue() {
+		return v;
+	}
+	
+
 }
